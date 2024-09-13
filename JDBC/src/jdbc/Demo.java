4ed9ba967b961fa02e8020e89efa2c4a9489bfb9
RAMPRAKASH81243 <68095922+RAMPRAKASH81243@users.noreply.github.com>
2024-09-13 11:55:04 +0530

    Update Demo.java

--- JDBC/src/jdbc/Demo.java
+++ JDBC/src/jdbc/Demo.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Demo {
 
 	public static void main(String[] args) throws Exception {
-		String url ="jdbc:mysql://programiz.com/sql/online-compiler/";
+		String url ="jdbc:mysql://localhost/3303/demo";
 		String uname="root";
 		String pass="root";
 		//Class.forName("com.mysql.cj.jdbc.Driver");
